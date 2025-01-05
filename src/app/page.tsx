450c93ac9d325e548360540436d70255e38c0186
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-01-05 14:48:21 +0600

    remove heading from base route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import WebHostingCollapse from "@/components/HomePage/WebHostingCollapse";
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
       <HeroCarousel />
       <ServicesTab />
       <CategoryCards />
